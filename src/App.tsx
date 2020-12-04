31feaaaca7322951f87a3c2ba27d1d7553663ef0
gamersolobr <gamerbrsolo@gmail.com>
2020-12-04 00:26:31 -0300

    Add JSON stringify

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   const token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJzYXltb25vbUBnbWFpbC5jb20iLCJzY29wZSI6WyJhbGwiXSwiZXhwIjoxNjA3MDUyMjMyLCJqdGkiOiI2ZDMwOTEwYi0yZmY0LTQ1YTgtYTE3Ni1lMGZjYjNhMzkzOTciLCJjbGllbnRfaWQiOiIyVDAyVU9rRjliazVTajRwIn0.f2EIF9RuvUhvTsdLAO2WR1nguyd2xMo2Wx6d-x5qSyjp_ljyLe_L3JwR_Oa7dPsgPQSanhxn3IEbHPIpkJFFQEp7FihKpejB3YCQGxQ-YDzEEqUUll5-A-KwYDfBpcjVkvL5jYWU49gb7K8H1NiRFKtThTiFglPzOJTYcXlHEf68lTndC-uYHsQq38_k5MVSOHSZ3YoxuQXvkrkZEoeM4zyNJfPhmGClpH2hcpLPfzNQjLKSvcBNbLUGk4OM7-1JPKZzD6fk7x_50odDQ4xI0gzjlIEGki7S8kuLhedQkMn8_Kaco6Pfy48OyfTXSlipV3mewm6xohsegMdfhVUdyA"
 
   const handleBillet = useCallback(async () => {
-    const data = {
+    const data = JSON.stringify({
       charge: {
         description: "1 ok",
         totalAmount: 9.00,
@@ -26,7 +26,7 @@ function App() {
         phone: "63992595358",
         notify: false
       }
-    }
+    })
 
     api.defaults.headers.Authorization = `Bearer ${token}`;
 
